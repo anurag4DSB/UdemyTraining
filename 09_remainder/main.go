4ede7583108db01948d4deea0c34b6f0fab20376
anurag4dsb <anurag.mittal@scality.com>
2018-09-20 08:05:53 -0700

    added lop and conditionals to remainder

--- 09_remainder/main.go
+++ 09_remainder/main.go
@@ -5,4 +5,17 @@ import "fmt"
 func main() {
 	x := 13 % 3
 	fmt.Println(x)
+	if x == 1 {
+		fmt.Println("Odd")
+	} else {
+		fmt.Println("Even")
+	}
+
+	for i := 1; i < 100; i++ {
+		if i%2 == 1 {
+			fmt.Println("Odd\t" , i)
+		} else {
+			fmt.Println("Even\t", i)
+		}
+	}
 }
