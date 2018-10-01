dda86ad8bf03444970623120ffb65e87f81191b7
anurag4dsb <anurag.mittal@scality.com>
2018-10-01 12:45:11 -0700

    slices

---
+++ 18_slice/07_append-invalid/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	greeting := make([]string, 3, 5)
+	// 3 is length - number of elements referred to by the slice
+	// 5 is capacity - number of elements in the underlying array
+
+	greeting[0] = "Good morning!"
+	greeting[1] = "Bonjour!"
+	greeting[2] = "buenos dias!"
+	greeting[3] = "suprabadham"
+
+	fmt.Println(greeting[2])
+}
+
+
+// This wont work as we have a length of 3 so on index 3 we need to append and not assign.
---
+++ 18_slice/12_multi-dimensional/01_shorthand-slice/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	student := []string{}
+	students := [][]string{}
+	fmt.Println(student)
+	fmt.Println(students)
+	fmt.Println(student == nil)
+}
+
+
+// var student []string will make it nil
