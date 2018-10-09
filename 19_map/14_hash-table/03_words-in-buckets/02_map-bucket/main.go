047f3f6bc67aba40feb1164ef83e716ef2bdf28a
anurag4dsb <anurag.mittal@scality.com>
2018-10-08 17:46:48 -0700

    maps and hash tables

--- 19_map/14_hash-table/03_words-in-buckets/02_map-bucket/main.go
+++ 19_map/14_hash-table/03_words-in-buckets/02_map-bucket/main.go
@@ -1,1 +1,48 @@
-package _2_map_bucket
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("http://www.gutenberg.org/cache/epub/1661/pg1661.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(res.Body)
+	defer res.Body.Close()
+
+	scanner.Split(bufio.ScanWords)
+	// Create map with a key of int
+	// and a value of another map
+	// with a key of string, which will be the word
+	// and a value of int, which will be the number of times the word occurs
+	buckets := make(map[int]map[string]int)
+	// Create slices to hold words words
+	for i := 0; i < 12; i++ {
+		buckets[i] = make(map[string]int)
+	}
+	for scanner.Scan() {
+		word := scanner.Text()
+		n := hashBucket(word, 12)
+		buckets[n][word]++
+	}
+	// Print words in a bucket
+	for k, v := range buckets[6] {
+		fmt.Println(v, "\t-", k)
+	}
+	fmt.Println(len(buckets))
+	fmt.Println(len(buckets[6]))
+}
+
+func hashBucket(word string, buckets int) int {
+	var sum int
+	for _, v := range word {
+		sum += int(v)
+	}
+	return sum % buckets
+
+}
