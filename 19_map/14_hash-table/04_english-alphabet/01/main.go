436b9eb788b291160e6484302fecc95dc23f2184
anurag4dsb <anurag.mittal@scality.com>
2018-10-08 17:59:35 -0700

    maps

--- 19_map/14_hash-table/04_english-alphabet/01/main.go
+++ 19_map/14_hash-table/04_english-alphabet/01/main.go
@@ -1,1 +1,18 @@
-package _1
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("http://www-01.sil.org/linguistics/wordlists/english/wordlist/wordsEn.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	bs, _ := ioutil.ReadAll(res.Body)
+	str := string(bs)
+	fmt.Println(str)
+}
