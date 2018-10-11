0ba5c0f3df867d89170cd8919dfa44008c25ed50
anurag4dsb <anurag.mittal@scality.com>
2018-10-11 16:53:15 -0700

    concurrency

--- 21_interfaces/05_conversion-vs-assertion/02_assertion/01_non-interface-error-invalid-code/main.go
+++ 21_interfaces/05_conversion-vs-assertion/02_assertion/01_non-interface-error-invalid-code/main.go
@@ -1,16 +1,14 @@
 package main
 
-import "fmt"
-
-func main() {
-	name := "Sydney"
-	str, ok := name.(string)
-	if ok {
-		fmt.Printf("%q\n", str)
-	} else {
-		fmt.Printf("value is not a string\n")
-	}
-}
+//func main() {
+//	name := "Sydney"
+//	str, ok := name.(string)
+//	if ok {
+//		fmt.Printf("%q\n", str)
+//	} else {
+//		fmt.Printf("value is not a string\n")
+//	}
+//}
 
 
 //will give an error as name is not an interface.
